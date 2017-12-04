65f6fecd18e9ee8a630df02aa0f615ad85792366
Jimmy Westcott <jimmywestcott@Jimmys-MacBook-Pro.local>
2017-12-04 13:52:44 -0330

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
+import { Subject } from 'rxjs/subject';
+import 'rxjs/add/operator/map';
+import { Observable } from 'rxjs/Observable';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  eventsSubject: Subject<any>;
+  eventGroups$: Observable<any>;
+  ngOnInit() {
+    this.eventsSubject = new Subject<any>();
+
+    this.eventGroups$ = this.eventsSubject
+      .map((item) => {
+        console.log(item);
+      });
+
+    this.eventsSubject.next('test1');
+  }
+
+  sendtest() {
+    this.eventsSubject.next('test from button');
+  }
 }
